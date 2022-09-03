ee23919a60cd9fca82b1b428675f09c1f219b278
NileshDeshmukh09 <nilesdeshmukh0908@gmail.com>
2022-09-03 19:15:09 +0530

    added Production URL to COnfig

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 const Config = {
-    baseUrl: 'http://localhost:8888/ticketsmodule/api/v1',  
+    // baseUrl: 'http://localhost:8888/ticketsmodule/api/v1',  
+    baseUrl: 'https://ticket-creation-module-crm.herokuapp.com/ticketsmodule/api/v1',  
 };
 
 export default Config;
