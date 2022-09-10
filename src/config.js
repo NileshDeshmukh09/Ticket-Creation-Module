2674a1f97553bfe2634585f9995495131d64435c
NileshDeshmukh09 <nilesdeshmukh0908@gmail.com>
2022-09-10 21:53:23 +0530

    Switch baseurl to production

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const Config = {
-    baseUrl: 'http://localhost:8888/ticketsmodule/api/v1',  // Localhost
-    // baseUrl: 'https://ticket-creation-module-crm.herokuapp.com/ticketsmodule/api/v1',  // Production
+    // baseUrl: 'http://localhost:8888/ticketsmodule/api/v1',  // Localhost
+    baseUrl: 'https://ticket-creation-module-crm.herokuapp.com/ticketsmodule/api/v1',  // Production
 };
 
 export default Config;
